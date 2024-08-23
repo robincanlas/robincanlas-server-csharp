113e5b7d871781aceadc191c552ccf3a79351ff3
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2024-08-23 22:11:51 +0800

    initial commit

---
+++ RobinCanlas/RobinCanlas/Controllers/CountryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobinCanlas.Services;
+using System.Diagnostics;
+
+namespace RobinCanlas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        private readonly IPositionStackService _positionStackService;
+
+        public CountryController(IPositionStackService positionStackService)
+        {
+            _positionStackService = positionStackService;
+        }
+
+        [HttpGet("{country}")]
+        //[HttpGet]
+        public async Task<IActionResult> GetCountries(string country)
+        //public async Task<IActionResult> GetCountries()
+        {
+            //Debug.WriteLine("hellowww");
+            //return Ok("Hello World");
+            var result = await _positionStackService.GetCountries(country);
+
+            return Ok(result);
+        }
+    }
+}
